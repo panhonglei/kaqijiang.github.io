ff21c9f1a52028186b8f15ab8677b673d2a7970a
侯伟杰 <kaqijiang@sina.cn>
2018-05-09 14:39:50 +0800

    1

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,7 +34,7 @@ var clickHandler = function(k) {
     window['tag'+k].delay(50).fadeIn(350);
   }
 };
-for (var i = 1; i <= 7; i++) {
+for (var i = 1; i <= 8; i++) {
   $('#js-label' + i).on('click', clickHandler(i));
 }
 
